2872639513f031c7c955f2e3e63e9aaf2b5ca86d
Pariniti <pritymishra00@gmail.com>
2025-07-21 20:04:34 +0530

    deploy

--- app/db.py
+++ app/db.py
@@ -1,16 +1,23 @@
 from pymongo import MongoClient
+import certifi
 
-client = MongoClient("mongodb+srv://admin:123@cluster0.yi9f0sk.mongodb.net/")
+#client = MongoClient("mongodb://localhost:27017/")
+client = MongoClient("mongodb+srv://admin:123@cluster0.yi9f0sk.mongodb.net/", tls=True,
+    tlsCAFile=certifi.where())
 
 # Check and create database and collection if not exist
 db_name = "ecommerce"
+
+#collections
 product_collection_name = "products"
 order_collection_name = "orders"
+
 
 db = client[db_name]
 
 if product_collection_name not in db.list_collection_names():
     db.create_collection(product_collection_name)
+
 if order_collection_name not in db.list_collection_names():
     db.create_collection(order_collection_name)
 
